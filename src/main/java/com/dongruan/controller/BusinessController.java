fd78904fc0c9f94ab6fe1599b0951ddca7fdfbc7
thomas_glb <737218196_glb@qq.com>
2020-09-23 09:49:31 +0800

    Business基本完成

--- src/main/java/com/dongruan/controller/BusinessController.java
+++ src/main/java/com/dongruan/controller/BusinessController.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 
 /**
  * @author shkstart
@@ -23,37 +24,76 @@ public class BusinessController {
     @Autowired
     private BusinessServise businessServise;
 
+    /**
+     * id 获取商家信息
+     * @param id
+     * @return
+     */
     @GetMapping("/getbyid/{id}")
     public Msg getBusiness(@PathVariable("id")Integer id){
         Business business = businessServise.getBusinessById(id);
         return new Msg().success().add("business", business);
     }
 
+    /**
+     * 获取所有商家
+     * @return
+     */
     @GetMapping()
     public Msg getAllBusiness(){
         List<Business> businesses = businessServise.showAllBusniess();
-        return new Msg().success().add("businesses", businesses);
+        return Msg.success().add("businesses", businesses);
     }
 
-//    @PostMapping("picture")
-//    public String PicTest(@RequestParam("file") MultipartFile multipartFile) throws IOException {
-//        System.out.println(multipartFile.getOriginalFilename());
-//        multipartFile.transferTo(new File("E:\\图片资源\\600.jpg"));
-//        File file = new File("E:\\图片资源\\600.jpg");
-//        System.out.println(BaseUtils.getBase64String(file));
-//        return new String();
-//
-//    }
+    /**
+     * 添加商家
+     * @param
+     * @return
+     * @throws IOException
+     */
+    @PostMapping()
+    public Msg addbusiness(Business business,MultipartFile multipartFile )throws IOException {
+        //先把上传的文件保存到E盘这个目录下
+        String fileName = System.currentTimeMillis()+ new Random().nextInt(9999)+".jpg";
+        System.out.println(business);
+        multipartFile.transferTo(new File("E:\\图片资源\\"+fileName));
+        File file = new File("E:\\图片资源\\"+fileName);
+//        把文件转成base64编码的字符串
+        String base64String = BaseUtils.getBase64String(file);
+        business.setBusinessimg(base64String);
+        businessServise.addBusiness(business);
+        return Msg.success();
 
+    }
+
+    /**
+     * 通过分类获取商家
+     * @param type
+     * @return
+     */
     @GetMapping("getbytype/{type}")
     public Msg getBusinessByType(@PathVariable("type")String type){
         List<Business> businesses = businessServise.showBusinessbyBusinessExplain(type);
-        return new Msg().success().add("businesses", businesses);
+        return Msg.success().add("businesses", businesses);
     }
 
 
-    @PostMapping()
-    public String ttt(){
-        return "ok";
+    @PutMapping
+    public Msg updateBusiness(Business business,MultipartFile multipartFile) throws IOException {
+        //先把上传的文件保存到E盘这个目录下
+        String fileName = System.currentTimeMillis()+ new Random().nextInt(9999)+".jpg";
+        multipartFile.transferTo(new File("E:\\图片资源\\"+fileName));
+        File file = new File("E:\\图片资源\\"+fileName);
+        //把文件转成base64编码的字符串
+        String base64String = BaseUtils.getBase64String(file);
+        business.setBusinessimg(base64String);
+        businessServise.updataBusiness(business);
+        return Msg.success();
+
+    }
+    @DeleteMapping("{id}")
+    public Msg deleteBusiness(@PathVariable("id") Integer businessid){
+        businessServise.deleteBusiness(businessid);
+        return Msg.success();
     }
 }
--- src/main/java/com/dongruan/elm/ElmApplication.java
+++ src/main/java/com/dongruan/elm/ElmApplication.java
@@ -1,23 +1,16 @@
 package com.dongruan.elm;
 
-
-import com.dongruan.dao.BusinessMapper;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import javax.sql.DataSource;
 
 @SpringBootApplication
 @ComponentScan("com.dongruan")
 @MapperScan(value = "com.dongruan.dao")
 public class ElmApplication {
-
-
     public static void main(String[] args) {
         SpringApplication.run(ElmApplication.class, args);
     }
-
 }
